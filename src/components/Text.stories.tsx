2aec360726a338697e8aac16c869cf1f0027e8ad
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-11 23:34:19 -0300

    added Text component storyboo

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
+
+export default {
+    tittle: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem Ipsum'
+    },
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
