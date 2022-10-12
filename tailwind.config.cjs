deab89e0499858d83f2ca847bfb26206a1c40d36
Daniel Henrique <henriqueevaldo@outlook.com>
2022-10-11 23:09:31 -0300

    source: import design tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,26 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize:{
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#e1e1e6',
+      'gray-100': '#c4c4cc',
+      
+      'cyan-600': '#4dabf7',
+      'cyan-500': '#61dafb',
+      'cyan-300': '#61b1fb'
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
